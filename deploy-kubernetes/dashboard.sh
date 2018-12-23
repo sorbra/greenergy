8388c0d7cc276a1449819ad2f9136ad61e130c6b
Søren Brandt <sorbra@gmail.com>
2018-12-23 16:09:27 +0100

    checkpoint

---
+++ deploy-kubernetes/dashboard.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+az aks browse --resource-group K8S-AZURE-SOB-RG --name K8S-AZURE-SOB
--- emissions-api/generate.emissions-api.client.sh
+++ emissions-api/generate.emissions-api.client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Before running this script, ensure that the emissions-api server is running on localhost:5000
+
 cd "$(dirname "$0")"
 
 nswag swagger2csclient /input:http://localhost:5000/swagger/v0.2/swagger.json /output:emissions-api.testclient/EmissionsClient.cs /namespace:Greenergy.Emissions.API
