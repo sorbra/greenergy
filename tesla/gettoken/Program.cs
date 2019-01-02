baf5c3adb7644b3b975b6666769dc61ac90db185
Søren Brandt <sorbra@gmail.com>
2019-01-02 23:14:39 +0100

    Hidden password entry.

--- tesla/gettoken/Program.cs
+++ tesla/gettoken/Program.cs
@@ -14,11 +14,45 @@ namespace Greenergy.TeslaToken
             return AccessToken;
         }
 
+        static string ReadPassword()
+        {            
+            string password = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                // Backspace Should Not Work
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        password = password.Substring(0, (password.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine();
+                        return password;
+                    }
+                }
+            } while (true);
+        }
+
         static void Main(string[] args)
         {
-            string AccessToken = GetToken(args[0],args[1]).Result;
+            Console.Write("Email: ");
+            var email = Console.ReadLine();
 
-            Console.WriteLine(AccessToken);
+            Console.Write("Password: ");
+            var password = ReadPassword();
+
+            string AccessToken = GetToken(email, password).Result;
+
+            Console.WriteLine($"AccessToken: {AccessToken}");
         }
     }
 }
